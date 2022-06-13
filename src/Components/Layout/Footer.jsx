682f9d881adc4b68f57b23cc4be594ea3e37670f
umeshyadav4321 <80197485+umeshyadav4321@users.noreply.github.com>
2022-06-13 23:02:17 +0530

    added

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -10,144 +10,26 @@ import CircleIcon from '@mui/icons-material/Circle';
 
 export const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
+        <h3>SELF DRIVE CARS IN MUMBAI</h3>
+        <p>The financial capital of India, Mumbai is a city which never sleeps. Buzzing with activity, it also holds the distinction of being the entertainment hub of the country. With a beach in its backyard and the picturesque Western Ghats a stone’s throw away, the city attracts visitors from all over the word. From the Gateway of India which opens the doors to India’s mysteries, to the ancient caves of Elephanta Island, and from the iconic Stock Exchange to the many studios where Bollywood movies are made, the city offers a multitude of sights and experiences.
+The downside of life in Mumbai is the difficulty commuting poses. Overloaded public transport and increasing distances add to the stress of living in Mumbai. Whether you are a visitor to this melting-pot of cultures, or a resident of the city, it is important to have a self-drive car at your disposal to make the most of your time. If you do not plan to buy a car of your own, Zoomcar offers the perfect solution by making the process of self- drive car booking in Mumbai convenient and affordable. The Zoomcar app allows you the freedom of booking a car at your fingertips.</p>
+<br/>
+<h3>CAR ON RENT IN MUMBAI</h3>
+<p>On many occasions, working professionals and business persons want to commute to different parts of the city in the same day. Usually, they hire a chauffeur driven car and pay extra for the waiting charges. This option, while popular, is also more expensive. It is more economical to opt for a self-drive car rental in Mumbai. You can spend any amount of time at a meeting without having to worry about the waiting charges, and use the car to explore Mumbai after work at your leisure. Zoomcar also provides Mumbai airport taxi service, using which you can pick up one of our cars right at the airport when you land. We have a variety of cars in our fleet including luxury brands which makes it easier to book luxury cars on rent in Mumbai.</p>
+<br/>
+<h3>CAR RENTAL IN MUMBAI</h3>
+<p>An ideal way to relax after a stressful week at the office is to go on a long drive from Mumbai to Pune or from Mumbai to Nasik. Enjoy the drive on the wide 6-lane Mumbai – Pune expressway with your loved ones by taking a car on hire in Mumbai. There are many religious sites, parks, and museums in Nasik which makes it an ideal location to unwind.</p>
+<br />
+<h3>ABOUT ZOOMCAR</h3>
+<p>No more worries about petrol mileage, insurance, and car breakdowns! Zoomcar has enabled driving convenience for travellers around the country and is fast expanding its reach to cities including Ahmedabad, Bangalore, Chandigarh, Chennai, Coimbatore, Delhi-NCR, Hyderabad, Jaipur, Kochi, Kolkata, Ludhiana, Mangalore, Mumbai, Mysore, Pune, Siliguri and Vizag. 
+Self-drive cars from Zoomcar have given customers more control, privacy, and freedom. Book a self-drive car in any city you visit with the Zoomcar app on your phone and feel at home wherever you go. </p>
 
-    <div className={styles.gridWrapper}>
-        <div className={styles.grid}>
-            <div className={styles.leftgrid}>
-                <h5 className={styles.h5}>ROYAL BROTHERS</h5>
-                <hr className={styles.hr} />
-                <p className={styles.p}>support@royalbrothers.com</p>
-                <p className={styles.p}>+91923232423/+919565364734</p>
-            </div>
-
-
-
-            <div className={styles.leftgrid}>
-                <h5 className={styles.h5}>COMPANY</h5>
-                <hr className={styles.hr} />
-
-                <Link to="/" className={styles.none}> <p className={styles.p}>About Us</p> </Link>
-                <Link to="/" className={styles.none}> <p className={styles.p}>Blog</p> </Link>
-                <Link to="/" className={styles.none}> <p className={styles.p}>Careers</p> </Link>
-                <Link to="/" className={styles.none}> <p className={styles.p}>Contact Us</p> </Link>
-
-            </div>
-            <div className={styles.leftgrid}>
-                <h5 className={styles.h5}>POLICIES</h5>
-                <hr className={styles.hr} />
-
-            <Link to="/" className={styles.none}> <p className={styles.p}>Privacy Policies</p> </Link>
-                <Link to="/" className={styles.none}> <p className={styles.p}>Terms & Conditions</p> </Link>
-            </div>
-            <div className={styles.leftgrid}>
-            <h5 className={styles.h5}>QUICK LINKS</h5>
-            <hr className={styles.hr} />
-
-            <Link to="/" className={styles.none}> <p className={styles.p}>Tariff</p> </Link>
-                <Link to="/" className={styles.none}> <p className={styles.p}>Own a franchise</p> </Link>
-                <Link to="/" className={styles.none}> <p className={styles.p}>Earn with us</p> </Link>
-                <Link to="/" className={styles.none}> <p className={styles.p}>Indian bike routes</p> </Link>
-            </div>
-            <div className={styles.leftgrid}>
-            
-                <h5 className={styles.h5}>FOLLOW US</h5>
-                <hr className={styles.hr} />
-                
-                <div className={styles.follow}>
-                <a className={styles.a} target="_blank" href= "https://www.instagram.com/royalbrothersrentals/?hl=en"><InstagramIcon/></a>
-                <a className={styles.a} target="_blank" href= "https://www.facebook.com/RoyalBrothersBikeRentals/"><FacebookIcon/></a>
-                <a className={styles.a} target="_blank" href= "https://www.linkedin.com/company/royal-brothers/?originalSubdomain=in"><LinkedInIcon/></a>
-                <a className={styles.a} target="_blank" href= "https://www.youtube.com/channel/UCRGf98BhX09kyw5hKBYhsdw/videos?view=0&sort=dd&shelf_id=0"><YouTubeIcon/></a>
-    </div>
-            </div>
-            <div className={styles.leftgrid}>
-                <h5 className={styles.h5}>DOWNLOAD OUR APP</h5>
-                <hr className={styles.hr} />
-
-                <div className={styles.banners}>
-                   <a className={styles.a} target="_blank"  href="https://apps.apple.com/in/app/royal-brothers-bike-rentals/id1405554590"><img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/app_store-4407bc8ced0d52a50ff042ce51e32d51de3e11717955798ec54c6a28e7926c7c.png" alt="" /></a> 
-                   <a className={styles.a} target="_blank" href="https://play.google.com/store/apps/details?id=com.royalbrothers&hl=en_IN"><img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/play_store-057f9061b4cd0fd4953a485569c15e63fdf8d89b0bcc025c04cfb34d9e8cd571.png" alt="" /></a> 
-
-
-                </div>
-            </div>
-
-        </div>
-    </div>
+   
 
 
 
-    <div className={styles.footer2wrapper}>
-        <div className={styles.presence}>
-
-        <h1 className={styles.h1}>Our Presence</h1>
-        <ul className={styles.ul}>
-        <li className={styles.li}>Bikes for rent in Agra </li> 
-        <li className={styles.li}> Bikes for rent in Amritsar</li>
-        <li className={styles.li}>Bikes for rent in Andaman Nicobar</li>
-          
-        <li className={styles.li}>Bikes for rent in Bangalore</li>
-        <li className={styles.li}> Bikes for rent in Bangalore Airport</li>
-        <li className={styles.li}> Bikes for rent in Bangkok</li>
-        <li className={styles.li}>  Bikes for rent in Belagavi </li>
-        <li className={styles.li}>  Bikes for rent in Bhubaneswar</li>
-        <li className={styles.li}>  Bikes for rent in Ganapati pule</li>
-
-        <li className={styles.li}>Bikes for rent in Cochin</li>
-
-        <li className={styles.li}>Bikes for rent in Davanagere</li>
-        <li className={styles.li}>Bikes for rent in Delhi</li>
-
-
-        <li className={styles.li}>Bikes for rent in Guwahati</li>
-        <li className={styles.li}>Bikes for rent in Hubli-Dharwad</li>
-        <li className={styles.li}> Bikes for rent in Hyderabad</li>
-        <li className={styles.li}> Bikes for rent in Indore</li>
-        <li className={styles.li}> Bikes for rent in Jaipur</li>
-        <li className={styles.li}>Bikes for rent in Manali</li>
-
-        <li className={styles.li}>Bikes for rent in Calicut </li>
-        <li className={styles.li}>Bikes for rent in Chandigarh </li>
-        <li className={styles.li}>Bikes for rent in Chennai</li>
-        <li className={styles.li}>Bikes for rent in Chikmagalur</li>
-        <li className={styles.li}>Bikes for rent in Coorg - Kushalnagar</li> 
-        <li className={styles.li}>Bikes for rent in Coorg - Madikeri</li>
-        <li className={styles.li}>Bikes for rent in Gandhinagar </li>
-        <li className={styles.li}>Bikes for ret in Goa </li>
-        <li className={styles.li}>Bikes for rent in Guntur</li>
-        <li className={styles.li}>Bikes for rent in Jaisalmer</li>
-        <li className={styles.li}>Bikes for rent in Jodhpur</li>
-        <li className={styles.li}> Bikes for rent in Kolkata</li>
-        <li className={styles.li}>Bikes for rent in Konark</li>
-        <li className={styles.li}> Bikes for rent in Lucknow</li>
-        <li className={styles.li}>Bikes for rent in Mangalore</li>
-        <li className={styles.li}>Bikes for rent in Ooty</li>
-        <li className={styles.li}>Bikes for rent in Pondicherry</li>
-        <li className={styles.li}> Bikes for rent in Pune</li>
-        <li className={styles.li}> Bikes for rent in Puri</li>
-        <li className={styles.li}> Bikes for rent in Ranchi</li>
-
-        <li className={styles.li}>Bikes for rent in Sakleshpur </li>
-        <li className={styles.li}>Bikes for rent in Siliguri</li>
-        <li className={styles.li}>Bikes for rent in Srinagar</li>
-        <li className={styles.li}>Bikes for rent in Surat</li>
-        <li className={styles.li}> Bikes for rent in Tirupati</li>
-        <li className={styles.li}> Bikes for rent in Trivandrum</li>
-        <li className={styles.li}>  Bikes for rent in Udaipur</li>
-        <li className={styles.li}>Bikes for rent in Mohali</li>
-        <li className={styles.li}>Bikes for rent in Mount Abu </li>
-        <li className={styles.li}>Bikes for rent in Mumbai</li>
-        <li className={styles.li}>Bikes for rent in Mysore</li>
-        <li className={styles.li}>Bikes for rent in Udupi - manipal</li>
-        <li className={styles.li}>Bikes for rent in Vijayawada </li>
-        <li className={styles.li}>Bikes for rent in Vizag</li>
-        <li className={styles.li}>Bikes for rent in Wayanad</li>
-  
-  </ul>
-
-        </div>
-    </div>
+   
 
 
 
--- src/Page/HomeComponets/Home.jsx
+++ src/Page/HomeComponets/Home.jsx
@@ -17,83 +17,20 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div >
       <div className={style.landing_container}>
         <div className={style.landing_image}> </div>
         <Form />
-
-        <div className={style.save}>
-          <span style={{ color: "#fed250" }}>Save</span> upto{" "}
-          <span style={{ color: "#fed250" }}>30%</span> with Royal Brother
-          <span style={{ color: "#fed250" }}>X</span>
-        </div>
       </div>
-      <div className={style.sanitized}>
-        <Senitized img={sanitized} text={"Sanitized Vehicles"} />
-        <Senitized img={umbreall} text={"Vehicle Insurance"} />
-        <Senitized img={tool} text={"24/7 Roadside Assistance"} />
-        <Senitized img={tools1} text={"Bike Maintenance"} />
-      </div>
-      <div className={style.rbx}>
-        <button className={style.subscribe}>Subscribe now </button>
-      </div>
+    
       <div>
-        <div className={style.fleet}>
-          <div>
-            <h1 style={{ color: "black" }}>OUR FEET</h1>
-            <Line />
-          </div>
-          <div className={style.fleetDuration}>
-            <label htmlFor="">Duration </label>
-            <select name="hours" id="">
-              <option value="">Hours</option>
-              <option value="">7 Days</option>
-              <option value="">15 Days</option>
-              <option value="">1 months</option>
-            </select>
-          </div>
-          <div className={style.fleet_card_container}>
-            <OurFleetCard
-              img={HomeBike1}
-              text={"Honda Dio"}
-              price={"$1/Hours"}
-            />
-            <OurFleetCard
-              img={HomeBike2}
-              text={"Honda Activa 5 G"}
-              price={"$2/Hours"}
-            />
-            <OurFleetCard
-              img={HomeBike3}
-              text={"Royal EndField 350"}
-              price={"$3/Hours"}
-            />
-            <div  >
-              <div className={style.add_more}>
-                <Link to="/searchR">
-                <img
-                  src={add}
-                  onClick={() => {
-                    console.log("aman");
-                  }}
-                  alt=""
-                />
-                </Link>
-              </div>
-              <OurFleetCard
-                img={HomeBike3}
-                text={"Royal EndField 350"}
-                price={"$3/Hours"}
-              />
-            </div>
-          </div>
-        </div>
+      <div className={style.landing_image2}> </div>
+      <div className={style.landing_image3}> </div>
+      <div className={style.landing_image4}> </div>
       </div>
-      <div className={style.slider_com}>
-        <h1 style={{ color: "black" }}>WE BELIEVE IN QUALITY</h1>
-        <Slider />
-      </div>
+
     </div>
+
   );
 };
 export default Home;
